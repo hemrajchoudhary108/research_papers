413b8479d5fe4537bd3b326e4abdaf354874962b
hemrajchoudhary7 <hemraj.rudra@gmail.com>
2024-12-09 15:00:00 +0530

    Batch Normalization paper added

--- research_papers.py
+++ research_papers.py
@@ -1,5 +1,8 @@
 paper_list = [
     {
-        'Title': 'Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift'
-    }
+        'Title': 'Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift',
+        'arxiv_link':'https://arxiv.org/abs/1502.03167',
+        'pdf_link': 'https://arxiv.org/pdf/1502.03167.pdf'
+    },
+    
 ]
